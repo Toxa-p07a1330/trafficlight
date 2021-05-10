b34fcdeb7837b353639d078af74ca5ed50558e9f
Toxa-p07a1330 <p07a1330@yandex.ru>
2021-05-10 22:20:13 +0300

    with mobile

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,8 @@ import Switches from "./Switches";
 import React from "react";
 import Content from "./Content";
 import Menu from "./Menu";
+import { useState } from 'react';
+
 
 function browser()
 {
@@ -23,19 +25,53 @@ function browser()
     return 'Search Bot';
 }
 
+function mobile(){
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    let div = height/width;
+    const standard = 1;
+    return div>standard;
+}
+
 let brsr = browser();
+let isMobile = mobile();
+let showMenuMobile = false;
 if (brsr === "Firefox" || brsr === 'Internet Explorer')
-alert("Ваш браузер не поддерживается");
+    alert("Ваш браузер не поддерживается");
+
 function App() {
+    const [showMenuMobile, setShowMenuMobile] = useState(false);
     let style = {
         width:"20%",
         display: "inine-block",
         float: "left"
     }
+function getMenu(isMobile){
+        if (!isMobile)
+            return <Menu/>
+        return <div>
+            <div style={{
+                position: "absolute",
+                fontSize: "25px",
+            }}
+            onClick={()=>{
+                   setShowMenuMobile(!showMenuMobile);
+                }}>
+                ≡
+             </div>
+            <div>
+                {showMenuMobile?<Menu/>:""}
+            </div>
+        </div>
+}
   return (
    <div align={"center"} style={{backgroundColor: "#cccccc"}}>
-    <Menu/>
-    <Content/>
+       <div>
+           {getMenu(isMobile)}
+       </div>
+            <div id = "content">
+                <Content/>
+            </div>
    </div>
   );
 }
